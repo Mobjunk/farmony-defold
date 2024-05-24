71018f07cc86774e2c780ba14e8a7133e78e6767
Mobjunk <34821598+Mobjunk@users.noreply.github.com>
2024-05-24 22:49:15 +0200

    Shirts Impl

--- modules/character/character_looks.lua
+++ modules/character/character_looks.lua
@@ -1,11 +1,11 @@
 local character_looks = {}
 
-character_looks.skin_color = 5
-character_looks.hair_color = 12
-character_looks.shirt_color = 1
-character_looks.pants_color = 2
-character_looks.eyes_color = 3
-character_looks.shoes_color = 4
+character_looks.skin_color = 0
+character_looks.hair_color = 0
+character_looks.shirt_color = 0
+character_looks.pants_color = 0
+character_looks.eyes_color = 0
+character_looks.shoes_color = 0
 
 character_looks.colors = {
     character_looks.skin_color,
@@ -16,10 +16,12 @@ character_looks.colors = {
     character_looks.hair_color
 }
 
-character_looks.hair_style = 2
+character_looks.hair_style = 0
+character_looks.shirt_style = 0
 
 character_looks.style = {
-    character_looks.hair_style
+    character_looks.hair_style,
+    character_looks.shirt_style
 }
 
 return character_looks;
