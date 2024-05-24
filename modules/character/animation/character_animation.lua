b712e6fcafd86d60ca2cda9e41901d5ceec93422
Mobjunk <34821598+Mobjunk@users.noreply.github.com>
2024-05-24 19:06:10 +0200

    Character Setup
    - Character is now setup properly for all the different types (hair, eyes, shirt, pants and shoes)
    - Has support for proper tools (pickaxe has been added for now)

--- modules/character/animation/character_animation.lua
+++ modules/character/animation/character_animation.lua
@@ -2,74 +2,84 @@ local character_info = require('modules.character.character_information')
 
 local character_animation = {}
 
-character_animation.current_animation = nil
-character_animation.animation_playing = false
-
-function character_animation.reset_animation(url, default, no_debug)
-	if not no_debug then
-		print('reset_animation ' .. url)
-	end
-    go.set(url, "image", default)
-	character_animation.animation_playing = false
-end
-
-function character_animation.play_animation(url)
-	local directions = {
-		"walk_right", "walk_left", "walk_up", "walk_down",
-		"idle_right", "idle_left", "idle_up", "idle_down"
+function character_animation.new()
+	local instance = {
+		current_animation = nil,
+		animation_playing = false
 	}
-	local animations = {}
-	
-	for _, direction in ipairs(directions) do
-		animations[direction] = {}
-		for _, color in ipairs(character_info.looks.colors) do
-			table.insert(animations[direction], hash(direction .. "_" .. color))
+
+	function instance.reset_animation(url, default, no_debug)
+		if not no_debug then
+			print('reset_animation ' .. url)
+		end
+		go.set(url, "image", default)
+		instance.animation_playing = false
+	end
+
+	function instance.play_animation(url, direction)
+		--print('trying to animate: ',url)
+		local directions = {
+			"walk_right", "walk_left", "walk_up", "walk_down",
+			"idle_right", "idle_left", "idle_up", "idle_down"
+		}
+		local animations = {}
+		
+		for _, direction in ipairs(directions) do
+			animations[direction] = {}
+			for _, color in ipairs(character_info.looks.colors) do
+				table.insert(animations[direction], hash(direction .. "_" .. color))
+			end
+		end
+
+		local anim = animations["idle_" .. character_info.direction][1]
+		local input = false--direction ~= nil and direction.x ~= 0 or direction.y ~= 0
+		if direction ~= nil then
+			input = direction.x ~= 0 or direction.y ~= 0
+		end
+
+		if not input then
+			if character_info.direction == 'right' then
+				anim = animations["idle_right"][1]
+			elseif character_info.direction == 'left' then
+				anim = animations["idle_left"][1]
+			elseif character_info.direction == 'up' then
+				anim = animations["idle_up"][1]
+			elseif character_info.direction == 'down' then
+				anim = animations["idle_down"][1]
+			end
+		else
+			if direction.x > 0 then
+				anim = animations["walk_right"][1]
+			elseif direction.x < 0 then
+				anim = animations["walk_left"][1]
+			elseif direction.y > 0 then
+				anim = animations["walk_up"][1]
+			elseif direction.y < 0 then
+				anim = animations["walk_down"][1]
+			end
+		end
+
+		if anim == instance.current_animation and input then
+			return;
+		end
+
+		msg.post(url, "play_animation", { id = anim })
+		instance.current_animation = anim
+	end
+
+	function instance.start_animation(url, animation)
+		go.set(url, "image", animation)
+		instance.play_animation(url)
+		instance.animation_playing = true
+	end
+
+	function instance.on_message(message_id, url, default)
+		if message_id == hash("animation_done") then
+			instance.reset_animation(url, default, true)
 		end
 	end
-	
-	local anim = animations["idle_" .. character_info.direction][1]
-	local input = character_info.walking_dir.x ~= 0 or character_info.walking_dir.y ~= 0
 
-	if not input then
-		if character_info.walking_dir.x > 0 then
-			anim = animations["idle_right"][1]
-		elseif character_info.walking_dir.x < 0 then
-			anim = animations["idle_left"][1]
-		elseif character_info.walking_dir.y > 0 then
-			anim = animations["idle_up"][1]
-		elseif character_info.walking_dir.y < 0 then
-			anim = animations["idle_down"][1]
-		end
-	else
-		if character_info.walking_dir.x > 0 then
-			anim = animations["walk_right"][1]
-		elseif character_info.walking_dir.x < 0 then
-			anim = animations["walk_left"][1]
-		elseif character_info.walking_dir.y > 0 then
-			anim = animations["walk_up"][1]
-		elseif character_info.walking_dir.y < 0 then
-			anim = animations["walk_down"][1]
-		end
-	end
-
-	if anim == character_animation.current_animation and input then
-		return;
-	end
-
-	msg.post(url, "play_animation", { id = anim })
-	character_animation.current_animation = anim
-end
-
-function character_animation.start_animation(url, animation)
-    go.set(url, "image", animation)
-	character_animation.play_animation(url)
-	character_animation.animation_playing = true
-end
-
-function character_animation.on_message(message_id, url, default)
-	if message_id == hash("animation_done") then
-		character_animation.reset_animation(url, default)
-	end
+	return instance
 end
 
 return character_animation
--- modules/character/character_information.lua
+++ modules/character/character_information.lua
@@ -2,7 +2,6 @@ local character_looks = require('modules.character.character_looks')
 
 local character_info = {}
 
-character_info.walking_dir = vmath.vector3()
 character_info.direction = 'down'
 character_info.looks = character_looks
 
